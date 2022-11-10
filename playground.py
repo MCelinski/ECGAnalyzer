809d35751891fc0508731d5ff93b443a658596e3
Michał Celiński <Michal.Celinski@ttms.pl>
2022-11-10 17:12:42 +0100

    requriments tests

--- playground.py
+++ playground.py
@@ -1,26 +1,26 @@
-# import wfdb 
-# import pandas as pd
-# from wfdb import processing
-# record = wfdb.rdrecord('signals/mitdb/103', sampto=3000)
-# annotation = wfdb.rdann('signals/mitdb/103', 'atr', sampto=3000)
-# sig, fields = wfdb.rdsamp('signals/mitdb/103', channels=[0])
+import wfdb 
+import pandas as pd
+from wfdb import processing
+record = wfdb.rdrecord('signals/103', sampto=3000)
+annotation = wfdb.rdann('signals/103', 'atr', sampto=3000)
+sig, fields = wfdb.rdsamp('signals/103', channels=[0])
 
-# wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
-#                    time_units='seconds', title='MIT-BIH Record 100',
-#                    figsize=(10,4), ecg_grids='all')
-# rrInterval = processing.calc_rr
-# distanceRR = processing.ann2rr('signals/mitdb/103', 'atr', as_array=True)
-# print(distanceRR)
-# maxRR = distanceRR.max()
-# minRR = distanceRR.min()
-# meanhr = processing.calc_mean_hr(distanceRR, fs =fields['fs'], min_rr = minRR, max_rr = maxRR,rr_units = 'samples')
-# print(meanhr)
-# df = pd.DataFrame(fields,columns="Heart Rate",dtype=float)
-# df.describe()
+wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
+                   time_units='seconds', title='MIT-BIH Record 100',
+                   figsize=(10,4), ecg_grids='all')
+rrInterval = processing.calc_rr
+distanceRR = processing.ann2rr('signals/103', 'atr', as_array=True)
+print(distanceRR)
+maxRR = distanceRR.max()
+minRR = distanceRR.min()
+meanhr = processing.calc_mean_hr(distanceRR, fs =fields['fs'], min_rr = minRR, max_rr = maxRR,rr_units = 'samples')
+print(meanhr)
+df = pd.DataFrame(fields,columns="Heart Rate",dtype=float)
+df.describe()
 
-# sig, fields = wfdb.rdsamp('signals/mitdb/101', channels=[0])
-# xqrs = processing.XQRS(sig=sig[:,0], fs=fields['fs'])
-# xqrs.detect()
+sig, fields = wfdb.rdsamp('signals/101', channels=[0])
+xqrs = processing.XQRS(sig=sig[:,0], fs=fields['fs'])
+xqrs.detect()
 
-# wfdb.plot_items(signal=sig, ann_samp=[xqrs.qrs_inds])
+wfdb.plot_items(signal=sig, ann_samp=[xqrs.qrs_inds])
 
