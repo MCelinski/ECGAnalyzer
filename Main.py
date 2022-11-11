39b833c8fddfd4534e4f6a5aaaece3bcb1827d43
MCelinski <mrcelus00@interia.pl>
2022-11-11 16:50:09 +0100

    added informatino about signal and analize window

--- Main.py
+++ Main.py
@@ -16,6 +16,7 @@ import tkinter.filedialog
 from tkinter import messagebox
 import os
 import wfdb
+from wfdb import processing
 import pandas as pd
 from scipy import signal
 
@@ -26,7 +27,7 @@ customtkinter.deactivate_automatic_dpi_awareness()
 
 class App(customtkinter.CTk):
 
-    WIDTH = 1024
+    WIDTH = 1224
     HEIGHT = 720
     def __init__(self):
         super().__init__()
@@ -46,7 +47,7 @@ class App(customtkinter.CTk):
 
         # configure grid layout (1x11)
         self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(6, weight=1)  # empty row as spacing
         self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
         
@@ -58,6 +59,7 @@ class App(customtkinter.CTk):
         self.add_analyze_image =self.load_image("/images/tools-icon.png",20)
         self.add_heart_image = self.load_image("/images/ecgg.png",120)
         self.add_info_image = self.load_image("/images/fileinfo.png",20)
+        self.add_xmark_image = self.load_image("/images/xmark.png",20)
         
 
  
@@ -76,12 +78,12 @@ class App(customtkinter.CTk):
         self.button_2.grid(row=3, column=0, pady=10, padx=20)
         
         self.button_3 = customtkinter.CTkButton(master=self.frame_left,image=self.add_analyze_image,compound="right", 
-                                                text="Filtruj sygnał      ",)
+                                                text="Analizuj sygnał     ",command=self.analize_signal)
         self.button_3.grid(row=4, column=0, pady=10, padx=20)
         
         self.button_4 = customtkinter.CTkButton(master=self.frame_left,image=self.add_info_image,compound="right", 
                                                 text="informacje o pliku",command=self.show_info)
-        self.button_4.grid(row=4, column=0, pady=10, padx=20)  
+        self.button_4.grid(row=5, column=0, pady=10, padx=20)  
         
         
         self.optionmenu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
@@ -96,10 +98,8 @@ class App(customtkinter.CTk):
         self.frame_right.columnconfigure(2, weight=0)
 
         self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
-        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=8, pady=20, padx=20, sticky="nsew")
         
-        
-
 
         # ============ frame_info ============
 
@@ -109,80 +109,52 @@ class App(customtkinter.CTk):
         self.frame_info.config
         
     def read_data(self):
+        try:
+              self.canvas.get_tk_widget().pack_forget()
+              self.toolbar.pack_forget()
+        except AttributeError:
+              pass
+        global ecg_path
         global signalSample
         global fields
-        global ann
+        global annotation
+        global record
         ecg_path = tkinter.filedialog.askopenfilename()
         ecg_path = ecg_path[:-4]
-        
-        input = customtkinter.CTkInputDialog(self, text="Ile próbek wyświetlić: ", title="Ilość próbek do wyświetlenia")
-        n_samples = int(input.get_input())
-        chanels=[1]
-        
-        record = wfdb.rdrecord(ecg_path, sampto=n_samples,channels=chanels)
-        annotation = wfdb.rdann(ecg_path, 'atr', sampto=3000)
 
-        return_fig = wfdb.plot_wfdb( record=record, annotation=annotation, plot_sym=True,
-        time_units='seconds', title='MIT-BIH Record 100',
-        figsize=(10,6), ecg_grids='all',return_fig=True)
+        
+        input = customtkinter.CTkInputDialog(self, text="Ile próbek wyświetlić\nWpisz '0' aby wyświetlić cały sygnał: ", title="Ilość próbek do wyświetlenia")
+        getedinput= input.get_input()
+        if getedinput == 'all':
+            signalSample,fields = wfdb.rdsamp(ecg_path, warn_empty=True)        
+            record = wfdb.rdrecord(ecg_path)
+            annotation = wfdb.rdann(ecg_path, 'atr')
+        else:    
+            
+            signalSample,fields = wfdb.rdsamp(ecg_path,sampto = int(getedinput), warn_empty=True)   
+            record = wfdb.rdrecord(ecg_path, sampto=int(getedinput))
+            annotation = wfdb.rdann(ecg_path, 'atr', sampto=int(getedinput))
+
+        return_fig = wfdb.plot_wfdb( record=record, annotation=annotation,plot_sym=True,
+                    time_units='seconds', title=ecg_path,
+                    figsize=(10,6), ecg_grids='all',return_fig=True)
         return_fig.set_facecolor("#F2F2F2")
-    
-        
-        
-
-        
-        
-        
-        #   n_samples = tkinter.simpledialog.askinteger('Liczba próbek', 
-        # 	# 'Wpisz ile próbek chcesz wyświetlić(przykład: 3000, 6000, 10000 etc.)')
-
-        # signalSample,fields = wfdb.rdsamp(ecg_path,sampto = n_samples,channels=[0], warn_empty=True)
-        # signal_list = signalSample.tolist()
-        
-        # ann = wfdb.rdann(ecg_path, 'dat', sampto=n_samples)
-        # annotation_time = (ann.sample / ann.fs)
-        # ann_sample = (ann.sample)
-        # ann_symbols = (ann.symbol)
-        # fs = fields.get('fs')      
-        # time =np.arange(n_samples) /fs
-       
-        # self.color =self.change_appearance_mode 
-        # f = Figure(figsize=(10,6), dpi=100,facecolor="#F2F2F2")
-        # a = f.add_subplot(111)
-        
-        # a.set_xlabel("time in s")
-        # a.set_ylabel("ECG in mV")
-        # a.set_title("ECG Signal")
-        # a.set_xlim(0, 10)
-        # a.grid()
-        # a.set_facecolor('#F2F2F2')
-        # a.plot(time,signal_list,'r')
-    
-
         
         self.canvas = FigureCanvasTkAgg(return_fig,master=self.frame_info)
-        
-        self.canvas.draw()
-        
+        self.canvas.draw() 
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=False)
+        
+        
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame_info)
         self.toolbar.config(background="#F2F2F2")
 
         self.toolbar.update()
-        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        # self.cursor = Cursor(a, horizOn=True, vertOn=True, useblit=True, color = 'r', linewidth = 1)
-        # annot = a.annotate("", xy=(0,0), xytext=(-40,40),textcoords="offset points",
-        #                 bbox=dict(boxstyle='round4', fc='linen',ec='k',lw=1),
-        #                 arrowprops=dict(arrowstyle='-|>'))
-        # annot.set_visible(False)
-        # self.coord = []
-        # signalSample.Get()
-        # fields.Get()
-        # ann.Get()
-                
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True) 
+        
+    
     def on_closing(self, event=0):
-        self.destroy()
+        self.quit()
         
     def change_appearance_mode(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -192,8 +164,10 @@ class App(customtkinter.CTk):
         return ImageTk.PhotoImage(Image.open(PATH + path).resize((image_size, image_size)))
 
     def show_info(self):
+        
+    
         window = customtkinter.CTkToplevel(self)
-        window.geometry("400x200")
+        window.geometry("600x200")
         window.title("Informacje o pliku")
         window.grid_columnconfigure(1, weight=1)
         window.grid_rowconfigure(0, weight=1)
@@ -206,39 +180,174 @@ class App(customtkinter.CTk):
          # ============ frame_left ============
 
         # configure grid layout (1x11)
-        window.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
+        window.frame_left.grid_rowconfigure(0, minsize=5)   # empty row with minsize as spacing
         window.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        window.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
-        window.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+        window.frame_left.grid_rowconfigure(8, minsize=10)    # empty row with minsize as spacing
+        window.frame_left.grid_rowconfigure(11, minsize=5)  # empty row with minsize as spacing
         
         window.frame_right = customtkinter.CTkFrame(master=window)
         window.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
-        
-        window.add_file_image = self.load_image("/images/filepropt.png", 40)
-        window.add_anotate_image = self.load_image("/images/anotate2.png", 20)
-    
-        
 
- 
+        window.add_file_image = self.load_image("/images/filepropt.png", 40)
         window.label_1 = customtkinter.CTkButton(master=window.frame_left,image=window.add_file_image,compound="bottom", 
                                                 corner_radius=10,
                                                 text="informacje o pliku",text_font=("Roboto Medium", -20))
-        window.label_1.grid(row=1, column=0, pady=10, padx=10)
+        window.label_1.grid(row=5, column=0, pady=10, padx=10)
         
-        # window.grid_columnconfigure(1, weight=1)
-        # window.grid_rowconfigure(0, weight=1)
-        # window.add_heart_image = self.load_image("/images/ecgg.png",30)
-        # text_var = tkinter.StringVar(value="częstość próbkowania:")
-        # window.label_1 = customtkinter.CTkButton(window,image=self.add_heart_image,compound="bottom", 
-        #                                         corner_radius=10,
-        #                                         text="ECG Analyzer",text_font=("Roboto Medium", -20))
-        # window.label_1.grid(row=1, column=0, pady=10, padx=10)
+        
+        
+        comment = fields['comments'][0]
+        age = comment[0:2]
+        gender = comment[3]
+        
+        if age == "-1":
+            age ="brak informacji"
+        if gender == "M":
+            gender ="Mężczyzna"
+        elif gender =="F":
+            gender ="Kobieta"
+        else:
+            gender="Brak Informacji"
+            
+            
+        window.label_info_1 = customtkinter.CTkLabel(master=window.frame_right,
+                                                   text=f"częsotliwość próbkowania: {fields['fs']} [Hz]",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="center",
+                                                   text_font=("Trebuchet MS",12))
+        window.label_info_1.grid(column=1, row=0, sticky="nwe", padx=5, pady=5)
+        
+        window.label_info_2 = customtkinter.CTkLabel(master=window.frame_right,
+                                                   text=f"Długość sygnału: {round(record.sig_len/record.fs,2)}[s]",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        window.label_info_2.grid(column=1, row=1, sticky="nwe", padx=5, pady=5)
+        
+        window.label_info_3 = customtkinter.CTkLabel(master=window.frame_right,
+                                                   text=f"Liczba Próbek: {round(record.sig_len)}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        window.label_info_3.grid(column=1, row=2, sticky="nwe", padx=5, pady=5)
+        
+        window.label_info_3 = customtkinter.CTkLabel(master=window.frame_right,
+                                                   text=f"Wiek: {age}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        window.label_info_3.grid(column=1, row=2, sticky="nwe", padx=5, pady=5)
+        
+        window.label_info_3 = customtkinter.CTkLabel(master=window.frame_right,
+                                                   text=f"Płeć: {gender}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        window.label_info_3.grid(column=1, row=3, sticky="nwe", padx=5, pady=5)
+   
+   
+    def analize_signal(self):
+        analyze = customtkinter.CTkToplevel(self)
+        analyze.geometry("600x200")
+        analyze.title("Analiza sygnału")
+        analyze.grid_columnconfigure(1, weight=1)
+        analyze.grid_rowconfigure(0, weight=1)
+ 
+        analyze.frame_left = customtkinter.CTkFrame(master=analyze,
+                                                 width=60,
+                                                 corner_radius=0)
+        analyze.frame_left.grid(row=0, column=0, sticky="nswe")
+ 
+         # ============ frame_left ============
 
-        # create label on CTkToplevel window
-        # label = customtkinter.CTkLabel(window, text="Informacje o pliku")
-        # label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
-        # print(signalSample)
+        # configure grid layout (1x11)
+        analyze.frame_left.grid_rowconfigure(0, minsize=5)   # empty row with minsize as spacing
+        analyze.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        analyze.frame_left.grid_rowconfigure(8, minsize=10)    # empty row with minsize as spacing
+        analyze.frame_left.grid_rowconfigure(11, minsize=5)  # empty row with minsize as spacing
+        analyze.add_analyze_image =self.load_image("/images/tools-icon.png",40)
+        analyze.frame_right = customtkinter.CTkFrame(master=analyze)
+        analyze.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
 
+        analyze.label_1 = customtkinter.CTkButton(master=analyze.frame_left,image=analyze.add_analyze_image,compound="bottom", 
+                                                corner_radius=10,
+                                                text="Analiza sygnału",text_font=("Roboto Medium", -20))
+        analyze.label_1.grid(row=5, column=0, pady=10, padx=10)
+        
+        
+        rr_array = wfdb.ann2rr(ecg_path, 'atr', as_array=True,format='s',stop_time=record.sig_len)
+        mean_rr = np.mean(rr_array)
+        mean_hr = processing.calc_mean_hr(rr_array,rr_units='seconds')
+    
+        
+
+            
+            
+        analyze.label_info_1 = customtkinter.CTkLabel(master=analyze.frame_right,
+                                                   text=f"Odstęp R-R:\n Max:{round(rr_array.max(),2)} [s] Min:{round(rr_array.min(),2)} [s] Mean:{round(mean_rr,2)} [s]",
+                                                   height=10,
+                                                   corner_radius=6,  
+                                                   fg_color=("white", "gray38"),  
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        analyze.label_info_1.grid(column=1, row=0, sticky="nwe", padx=5, pady=5)
+        
+        analyze.label_info_2 = customtkinter.CTkLabel(master=analyze.frame_right,
+                                                   text=f"Średnie HR: {round(mean_hr,3)}[Bpm]",
+                                                   height=10,
+                                                   corner_radius=6, 
+                                                   fg_color=("white", "gray38"),  
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        analyze.label_info_2.grid(column=1, row=1, sticky="nwe", padx=5, pady=5)
+        
+        analyze.label_info_3 = customtkinter.CTkLabel(master=analyze.frame_right,
+                                                   text=f"Liczba Próbek: {round(record.sig_len)}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        analyze.label_info_3.grid(column=1, row=2, sticky="nwe", padx=5, pady=5)
+        
+        analyze.label_info_3 = customtkinter.CTkLabel(master=analyze.frame_right,
+                                                   text=f"Wiek: {age}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        analyze.label_info_3.grid(column=1, row=2, sticky="nwe", padx=5, pady=5)
+        
+        analyze.label_info_3 = customtkinter.CTkLabel(master=analyze.frame_right,
+                                                   text=f"Płeć: {gender}",
+                                                   height=10,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT,
+                                                   anchor="w",
+                                                   text_font=("Trebuchet MS",12))
+        analyze.label_info_3.grid(column=1, row=3, sticky="nwe", padx=5, pady=5)
+    
 
 if __name__ == "__main__":
     app = App()
